8a2798d1cabcdf725a898b8d6948c81ff6e6bf2d
lucsleo123 <contaptudolucs@gmail.com>
2025-02-02 18:35:16 -0300

    frutas.cpp

--- frutas.cpp
+++ frutas.cpp
@@ -3,26 +3,57 @@
 #include <string>
 #include "frutas.hpp"
 
-int main() {
-    // Lê os dados do arquivo CSV
-    vector<Frutas> listaFrutas = Frutas::lerArquivoCSV("frutas.csv");
+Frutas::Frutas(string _nome, float _pAtual, float _pAnterior) {
+    
+    nome = _nome;
+    pAtual = _pAtual;
+    pAnterior = _pAnterior;
 
-    // Verifica se a lista não está vazia
-    if (listaFrutas.empty()) {
-        cout << "Nenhuma fruta encontrada no arquivo!" << endl;
-        return 1;
+}
+
+float Frutas::calcVariacaoPreco() const {
+
+    if (pAnterior == 0) return 0;  
+    return ((pAtual - pAnterior) / pAnterior) * 100;
+
+}
+
+vector<Frutas> Frutas::lerArquivoCSV(string nomeArquivo){
+
+    vector<Frutas> frutas;  
+    ifstream arquivo(nomeArquivo);
+    string linha, nome;
+    float pAtual, pAnterior;
+
+    if (!arquivo.is_open()) {
+        cerr << "Erro ao abrir o arquivo " << nomeArquivo << std::endl;
+        return frutas;
     }
 
-    // Encontra a fruta com maior e menor variação de preço
-    Frutas maiorFruta("", 0, 0);
-    Frutas menorFruta("", 0, 0);
-    Frutas::encontrarMaiorMenorVariacao(listaFrutas, maiorFruta, menorFruta);
+    getline(arquivo, linha);
 
-    // Exibe os resultados
-    cout << "Fruta com maior aumento de preço: " << maiorFruta.getNome() 
-         << " (R$ " << maiorFruta.variacaoPreco() << ")" << endl;
-    cout << "Fruta com menor aumento de preço: " << menorFruta.getNome() 
-         << " (R$ " << menorFruta.variacaoPreco() << ")" << endl;
+    while (getline(arquivo, linha)) {
+        stringstream ss(linha);
+        getline(ss, nome, ';');  
+        ss >> pAtual;          
+        ss.ignore();                  
+        ss >> pAnterior;             
+      
+        frutas.push_back(Frutas(nome, pAtual, pAnterior));
+    }
 
-    return 0;
+    arquivo.close();  
+    return frutas;
+
+}
+
+void Frutas::MaiorMenorExtremos(const vector<Frutas>& lista, Frutas& maiorFruta, Frutas& menorFruta) {
+    for (const Frutas& fruta : lista) {  // para nao modificar a lista
+        if (fruta.calcVariacaoPreco() > maiorFruta.calcVariacaoPreco()) {
+            maiorFruta = fruta;
+        }
+        if (fruta.calcVariacaoPreco() < menorFruta.calcVariacaoPreco()) {
+            menorFruta = fruta;
+        }
+    }
 }
